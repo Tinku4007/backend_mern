732f5a1708c909a2ef19e2e2a7d9f5a1e5d36daa
Impactmindz-Tech <impactmindztechsolutions@gmail.com>
2024-06-07 12:19:39 +0530

    update

--- index.js
+++ index.js
@@ -3,7 +3,12 @@ const cors = require('cors')
 const app = express()
 
 app.use(cors())
-app.use(express.json())
+app.use(express.json());
+
+
+app.get('/',(req,res)=>{
+    res.send("hello");
+})
 
 app.post('/signup', async (req, res) => {
     try {
